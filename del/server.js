5bd2318f92c3926fe4f514895ef8a5d370c5c1f6
Omar Mojica <omar@mojicalabs.com>
2022-09-12 23:54:50 -0400

    paths

--- del/server.js
+++ del/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('build'));
+app.use(express.static('dist'));
 app.use(express.static(path.join(__dirname, '/build')));
 
 app.use(function (req, res, next) {
